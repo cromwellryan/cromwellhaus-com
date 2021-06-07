d25727cf67336686a08d412683adf617634d39e4
Ryan Cromwell <ryan@heysparkbox.com>
2021-06-07 13:25:41 -0400

    Init .com

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
+import Image from 'next/image'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <header>
+        <div>
+          <a aria-label="Link to Home" title="Link to Home" href="/">
+            Ryan Cromwell
+          </a>
+        </div>
+        <nav>
+          <a href="/blog">Blog</a>
+        </nav>
+      </header>
+
+      <Component {...pageProps} />
+
+      <footer>
+      </footer>
+    </div>
+  );
 }
 
 export default MyApp
