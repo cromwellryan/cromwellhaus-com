90adb0d02b30a10bc169b3fee3081b08b83399ac
Ryan Cromwell <ryan@heysparkbox.com>
2021-06-09 20:19:30 -0400

    feat (Layout): Prefooter 🎉 with Cool tech thing and tweets

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,25 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </section>
 
+      <section className="prefooter">
+        <div className="onecoolthing">
+          <h3 className="onecoolthing__header">Cool <span className="util-highlight">Tech</span> Thing</h3>
+          <p className="onecoolthing__body">Way to get to the bottom. If you leave here with nothing else, check out this one cool thing.</p>
+        </div>
+
+        <div className="onecoolthing">
+          <h3 className="onecoolthing__header">Latest <span className="util-highlight">Tweet</span></h3>
+          <p className="onecoolthing__body">
+            <a className="twitter-timeline" href="https://twitter.com/cromwellryan" data-height="200" data-widget-id="368846062565347328" data-chrome="noheader nofooter noborders noscrollbar transparent" data-tweet-limit="1">Tweets by @cromwellryan</a>
+
+
+          </p>
+
+          <script async src="https://platform.twitter.com/widgets.js"></script>
+        </div>
+      </section>
+
+
       <footer>
         &copy;2021 Ryan Cromwell
       </footer>
