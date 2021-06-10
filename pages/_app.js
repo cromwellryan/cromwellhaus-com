ce5c8de82b68010f93a0d2c0bf5c4c95c33d24b1
Ryan Cromwell <ryan@heysparkbox.com>
2021-06-09 20:58:56 -0400

    feat (Prefooter): Tweets links to my twitter profile - being helpful

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import Image from 'next/image'
+import Prefooter from '../components/Prefooter'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,36 +13,21 @@ function MyApp({ Component, pageProps }) {
           <nav className="header__nav">
             <a className="nav__link" href="/blog">Blog</a>
           </nav>
-        </div>      </header>
+        </div>
+      </header>
 
       <section className="body-content">
         <Component {...pageProps} />
       </section>
 
-      <section className="prefooter">
-        <div className="onecoolthing">
-          <h3 className="onecoolthing__header">Cool <span className="util-highlight">Tech</span> Thing</h3>
-          <p className="onecoolthing__body">Way to get to the bottom. If you leave here with nothing else, check out this one cool thing.</p>
-        </div>
-
-        <div className="onecoolthing">
-          <h3 className="onecoolthing__header">Latest <span className="util-highlight">Tweet</span></h3>
-          <p className="onecoolthing__body">
-            <a className="twitter-timeline" href="https://twitter.com/cromwellryan" data-height="200" data-widget-id="368846062565347328" data-chrome="noheader nofooter noborders noscrollbar transparent" data-tweet-limit="1">Tweets by @cromwellryan</a>
-
-
-          </p>
-
-          <script async src="https://platform.twitter.com/widgets.js"></script>
-        </div>
-      </section>
+      <Prefooter />
 
 
       <footer>
         &copy;2021 Ryan Cromwell
       </footer>
     </div>
-  );
+  )
 }
 
 export default MyApp
