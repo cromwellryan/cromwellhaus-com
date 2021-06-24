c89e0441ce0144e45d821f157111cadaf7669753
Ryan Cromwell <ryan@heysparkbox.com>
2021-06-23 23:41:00 -0400

    fix (App): Head content in all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 import Image from 'next/image'
+
+import '../styles/globals.css'
 import Prefooter from '../components/Prefooter'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+
+        <title>Ryan Cromwell</title>
+        <meta name="description" content="Ryan Cromwell - Big Vision. Small Changes. Learning by shipping." />
+        <link href="https://twitter.com/cromwellryan" rel="me" />
+        <link href="https://github.com/cromwellryan" rel="me" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@cromwellryan" />
+        <meta name="twitter:title" content="Cromwellhaus.com" />
+        <meta name="twitter:description" content="Ryan Cromwell: Dad of 4. Technical Director @hearsparkbox. Big vision. Small changes. Always Learning." />
+
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+
+        <link rel='canonical' href="https://cromwellhaus.com" />
+      </Head>
+
       <header className="header">
         <div className="header__container">
           <a className="header__link-home" aria-label="Link to Home" title="Link to Home" href="/">
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-
 import ExternalContentArticle from '../components/ExternalContentArticle';
 
 import styles from '../styles/Home.module.css'
@@ -33,25 +31,6 @@ export default function Home({recentContent}) {
 
   return (
     <div>
-
-      <Head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
-
-        <title>Ryan Cromwell</title>
-        <meta name="description" content="" />
-        <link href="https://twitter.com/cromwellryan" rel="me" />
-        <link href="https://github.com/cromwellryan" rel="me" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@cromwellryan" />
-        <meta name="twitter:title" content="Cromwellhaus.com" />
-        <meta name="twitter:description" content="Ryan Cromwell: Dad of 4. Technical Director @hearsparkbox. Big vision. Small changes. Always Learning." />
-
-        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-
-        <link rel='canonical' href="https://cromwellhaus.com" />
-      </Head>
 
       <section className={styles.section}>
         {articles}
