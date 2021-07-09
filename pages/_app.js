d63c866d245d85cfcefb4e466443b5c79ed420e6
Ryan Cromwell <ryan@heysparkbox.com>
2021-07-09 11:58:06 -0400

    feat (Favicon): Green circle favicon for various resolutions

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,10 @@ function MyApp({ Component, pageProps }) {
         <meta name="twitter:title" content="Cromwellhaus.com" />
         <meta name="twitter:description" content="Ryan Cromwell: Dad of 4. Technical Director @hearsparkbox. Big vision. Small changes. Always Learning." />
 
-        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         <link rel='canonical' href="https://cromwellhaus.com" />
       </Head>
