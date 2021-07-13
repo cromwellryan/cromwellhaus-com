5b1a344b50ccee723d1918e17329240b7e40b63a
Ryan Cromwell <ryan@heysparkbox.com>
2021-07-12 22:45:01 -0400

    feat (Home): Indicate those publications that are external

--- components/ExternalContentArticle.js
+++ components/ExternalContentArticle.js
@@ -6,7 +6,7 @@ import styles from '../styles/Article.module.css'
 function ExternalContentArticle({fields}) {
   return (
     <article className={styles.article}>
-      <h2 className={styles.title}><a className={styles.link} href={fields.url}>{fields.title}</a></h2>
+      <h2 className={[styles.title, styles.externalAnnotation].join(' ')}><a className={styles.link} href={fields.url}>{fields.title}</a></h2>
       {documentToReactComponents(fields.abstract)}
     </article>
   );
